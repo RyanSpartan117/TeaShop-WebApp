27f61d7e13c4a87e60cdb1d82670a68c7bc57e44
Ryan Chapman <rchapman@spartaglobal.co>
2017-06-08 15:05:59 +0100

    working migration. Schema created. Bug fix- config.ru missed require sinatra/activerecords

--- db/migrate/20170608130753_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreateTeaStores < ActiveRecord::Migration[5.1]
-  def change
-  	create_table :tea_stores do |t|
-  		t.string :name, null:false
-  		t.string :location, null:false
-  		t.decimal :rating, null:false
-  end
-end
-
---
+++ db/migrate/20170608140114_tea_stores.rb
@@ -0,0 +1,10 @@
+class TeaStores < ActiveRecord::Migration[5.1]
+  def change
+  	create_table :tea_stores do |t|
+      t.string :name, null: false
+      t.string :location, null: false
+      t.decimal :rating, nul:false
+  	end
+ 
+  end
+end
