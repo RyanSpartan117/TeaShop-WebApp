b0e24bff135c3c17b83c08ed857bd42a21a55e1b
Ryan Chapman <rchapman@spartaglobal.co>
2017-06-12 09:50:02 +0100

    add tea fixed.

--- public/main.js
+++ public/main.js
@@ -5,11 +5,11 @@ function initMap() {
 
 	var uluru = {lat: latitude, lng: longitude};
 	var map = new google.maps.Map(document.getElementById('map-location'), {
-	  zoom: 13,
+	  zoom: 15,
 	  center: uluru
 	});
 	var marker = new google.maps.Marker({
 	  position: uluru,
 	  map: map
 	});
-}
+};
